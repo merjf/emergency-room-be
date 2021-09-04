b931745643089d73328d78ac1e1cdc520a869b11
Francesco Mergiotti <francescomergiotti.25@gmail.com>
2021-09-04 14:34:05 +0200

    Init Project

---
+++ src/main/java/com/portaleps/PortalepsApplication.java
@@ -0,0 +1,13 @@
+package com.portaleps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalepsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/portaleps/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.portaleps.repository;
+
+import com.portaleps.model.User;
+
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByUsername(String username);
+
+    User findByUsername(String username);
+
+    @Transactional
+    void deleteByUsername(String username);
+
+}
